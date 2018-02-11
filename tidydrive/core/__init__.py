3a70e379ae2ac533786b00d05805747fc283f635
Zeke Arneodo <earneodo@ucsd.edu>
2018-02-10 20:52:50 -0800

    Update __init__.py

--- tidydrive/core/__init__.py
+++ tidydrive/core/__init__.py
@@ -1,5 +1,5 @@
-from api import download_file
-from api import upload_file
-import util
+from .api import download_file
+from .api import upload_file
+from .util import path_parts
 
-__all__ =['download_file', 'upload_file]
+__all__ =['download_file', 'upload_file', 'path_parts']
