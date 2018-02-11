d07a8305fa572b2a0b1aa50793d3b570ec97a524
Zeke Arneodo <earneodo@ucsd.edu>
2018-02-11 11:49:48 -0800

    Update __init__.py

--- tidydrive/__init__.py
+++ tidydrive/__init__.py
@@ -1,6 +1,6 @@
 from .tidydrive import TidyDrive
 
-from ._api import dowlnoad_chunk, download_file, upload_file
+from ._api import download_chunk, download_file, upload_file
 from ._util import path_parts
 
 __all__ = ['TidyDrive', '._api', '._util']
